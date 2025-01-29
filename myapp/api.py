593be32e57c5172adb774f6b83e65d5a74101207
Mark Porter <mark.porter@gbatteries.com>
2025-01-28 21:43:38 -0500

    ok this query should work

--- myapp/api.py
+++ myapp/api.py
@@ -5,9 +5,10 @@ import requests as r
 def root():
     return {"message": "hello from reflex"}
 
+
 url = 'https://backboard.railway.com/graphql/v2'
 
-body = """ 
+body = """
 
     query MyQuery {
     projects {
@@ -21,13 +22,14 @@ body = """
 """
 
 
-
 def test():
-    
-    response = r.post(url=url,
-                  json={
-                      "query": body,
-                      "authorization": 'Bearer: 81335197-ccd2-484a-bf80-2edb17cdbcee'
-                  })
-        
+
+    response = response = r.post(
+        url=url,
+        data='{"query":"query { me { name email } }"}',
+        headers={
+            "Authorization": 'Bearer 81335197-ccd2-484a-bf80-2edb17cdbcee',
+            'Content-Type': 'application/json',
+        })
+
     return response
