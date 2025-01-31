cf8f83fe26a3cec9a5d142fa7c342ccb68442755
Mark Porter <mark.porter@gbatteries.com>
2025-01-30 19:49:44 -0500

    whoops re-update

--- rxconfig.py
+++ rxconfig.py
@@ -9,7 +9,7 @@ class ReflextemplateConfig(rx.Config):
     pass
 
 config = ReflextemplateConfig(
-    app_name="DashDeployer",
+    app_name="myapp",
     telemetry_enabled=False,
     frontend_port=3000, # default frontend port
     backend_port=8000, # default backend port
